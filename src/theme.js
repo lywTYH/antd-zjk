2cc5c0cca86bcd926f84f6062b48aa384267805f
lyw <763613923@qq.com>
2023-11-01 13:17:15 +0800

    chore: 0.1 version

---
+++ src/theme.js
@@ -0,0 +1,68 @@
+
+export const seedToken = {
+  blue: '#008ee0',
+  green: '#29bc38',
+  gold: '#ff9933',
+  red: '#f60a14',
+  colorPrimary: '#008ee0',
+  colorInfo: '#008ee0',
+  colorSuccess: '#29bc38',
+  colorWarning: '#ff9933',
+  colorError: '#ff3333',
+};
+
+export const mapToken = {
+  colorText: 'rgba(0, 0, 0, 0.85)',
+  colorBorderSecondary: '#e8e8e8',
+  colorFillSecondary: 'rgba(0, 0, 0, 0.09)',
+  fontSizeHeading2: 28,
+  fontSizeHeading1: 36,
+  lineHeightHeading1: '1.3888888888888888888888',
+  lineHeightHeading2: '1.357142857142857',
+}
+
+//组件主题
+export const ComponentsToken = {
+  Button: {
+    controlOutline: 'rgba(0, 142, 224, 0.09)',
+    colorErrorOutline: 'rgba(255, 51, 51, 0.09)',
+    controlTmpOutline: 'rgba(191, 191, 191, 0.09)',
+    controlHeightLG: 36,
+    borderRadius: 4,
+    borderRadiusLG: 6,
+    borderRadiusSM: 2,
+  },
+  Anchor: {
+    linkPaddingBlock: 8,
+    linkPaddingInlineStart: 12,
+  },
+  Breadcrumb: {
+    itemColor: 'rgba(0, 0, 0, 0.25)',
+    lastItemColor: 'rgba(0, 0, 0, 0.65)',
+    linkColor: 'rgba(0, 0, 0, 0.25)',
+    separatorColor: 'rgba(0, 0, 0, 0.25)',
+    linkHoverColor: 'rgba(0, 0, 0, 0.65)',
+    colorText: 'rgba(0, 0, 0, 0.65)',
+    colorTextDescription: 'rgba(0, 0, 0, 0.25)',
+  },
+  Dropdown: {
+    paddingXXS: 2,
+    marginXXS: 2,
+    marginXS: 6,
+    paddingXS: 4,
+  },
+  Pagination: {
+    itemSizeSM: 20,
+    controlHeightSM: 20,
+    borderRadiusSM: 2,
+    borderRadius: 4,
+  },
+  Steps: {
+    colorFillContent: 'rgba(0, 0, 0, 0.04)',
+    controlItemBgActive: 'rgba(0, 142, 224, 0.09)',
+    dotSize: 6,
+    dotCurrentSize: 8,
+    fontSize: 14,
+    titleLineHeight: 28,
+  },
+};
---
+++ work.md
@@ -0,0 +1,16 @@
+1. copy es antd to dist   success
+2. overwrite seedToken    success
+3. overwrite mapToken     success
+4. overwrite componentToken success
+
+## high priority
+  + 4.1 replace rollup 
+     替换 rollup，rollup 作为一个打包工具并不适合组件开发，打包后 tree shaking 支持度低还失去 ui 组件良好的结构。
+     考虑到实际工作并不需要 unpkg, 意义太低。
+  + 整理依赖 
+
+5. check type
+6. work as npm 
+
+7. add  custom component 
+8. add doc 
